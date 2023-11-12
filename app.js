7a94e053ccc465e1e202aab722548aa2fa78a417
Ivan <120274876+Gelsomin0@users.noreply.github.com>
2023-11-12 18:30:39 +0200

    Auth docs are almoust done

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
 import 'dotenv/config';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json' assert {type: 'json'};
 
 import authRouter from "./routes/api/auth.js";
 import recordsRouter from './routes/api/records.js';
@@ -16,6 +18,7 @@ app.use(express.json());
 
 app.use("/api/users", authRouter);
 app.use("/api/records", recordsRouter);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
